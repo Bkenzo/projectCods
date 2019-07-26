2b78611671908de82e7cf243cf7ef96834e4dceb
alex_sousa_alves <alexsousaalves317@gmail.com>
2019-07-26 00:00:42 -0300

    list completed

--- src/js/script.js
+++ src/js/script.js
@@ -2,36 +2,24 @@ var fieldFirstName = document.querySelector(".fieldFirstName");
 
 var buttonSend = document.querySelector(".buttonSend");
 
-var buttonDisplayPeoples = document.querySelector(".buttonDisplayPeoples");
-
 var divContainer = document.querySelector(".container");
 
 var formRecordPeople = document.querySelector(".recordPeople");
 
-var peoples = [];
-
 var ulAttribute = document.createElement("ul");
 
+ulAttribute.className = "listPeoples";
 
+divContainer.appendChild(ulAttribute);
 
 buttonSend.addEventListener("click", function (event) {
     event.preventDefault();
-    peoples.push(fieldFirstName.value);
-    console.log(peoples);
-})
 
-buttonDisplayPeoples.addEventListener("click", function (event) {
-    // event.preventDefault();
-
-    ulAttribute.className = "listPeoples";
-    divContainer.appendChild(ulAttribute);
     var liAttribute = document.createElement("li");
-    divContainer.appendChild(ulAttribute);
-    
-    peoples.forEach((people) => {
-        liAttribute.textContent = people;
-        ulAttribute.appendChild(liAttribute);
-    })
 
+    liAttribute.textContent = fieldFirstName.value;
+    
+    ulAttribute.appendChild(liAttribute);
+    
 })
 
